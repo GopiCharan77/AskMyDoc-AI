cc22c2decb5c94dce3ccc7d687432c70829905aa
Grecil <grecilunadkat@gmail.com>
2026-05-13 23:13:03 +0530

    Revert the changes made to embedding

--- models/EM.py
+++ models/EM.py
@@ -2,5 +2,5 @@ from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 import os
 
 embedding = GoogleGenerativeAIEmbeddings(
-    model="gemini-embedding-2", google_api_key=os.environ.get("GEMINI_API_KEY")
+    model="models/text-embedding-004", google_api_key=os.environ.get("GEMINI_API_KEY")
 )
--- tools/index_tool.py
+++ tools/index_tool.py
@@ -3,16 +3,11 @@ from langchain_community.vectorstores.faiss import FAISS
 from langchain_community.document_loaders import PyMuPDFLoader
 from models.EM import embedding
 
+
 def indexer(file):
     docs = PyMuPDFLoader(file).load()
     splits = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=1024, chunk_overlap=128
     ).split_documents(docs)
-    
-    batch_size = 16
-    vectorstore = FAISS.from_documents(splits[:batch_size], embedding)
-    
-    for i in range(batch_size, len(splits), batch_size):
-        vectorstore.add_documents(splits[i:i+batch_size])
-        
+    vectorstore = FAISS.from_documents(splits, embedding)
     vectorstore.save_local("index")
