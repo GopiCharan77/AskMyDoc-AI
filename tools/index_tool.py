f1ce3705c0d311f11e66537c67cfe7b8d53e189b
Grecil <grecilunadkat@gmail.com>
2026-05-13 23:02:01 +0530

    embed the documents in batches

--- tools/index_tool.py
+++ tools/index_tool.py
@@ -3,11 +3,16 @@ from langchain_community.vectorstores.faiss import FAISS
 from langchain_community.document_loaders import PyMuPDFLoader
 from models.EM import embedding
 
-
 def indexer(file):
     docs = PyMuPDFLoader(file).load()
     splits = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=1024, chunk_overlap=128
     ).split_documents(docs)
-    vectorstore = FAISS.from_documents(splits, embedding)
+    
+    batch_size = 16
+    vectorstore = FAISS.from_documents(splits[:batch_size], embedding)
+    
+    for i in range(batch_size, len(splits), batch_size):
+        vectorstore.add_documents(splits[i:i+batch_size])
+        
     vectorstore.save_local("index")
